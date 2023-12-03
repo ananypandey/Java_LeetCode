72f7a4814c65e3b8918206b9030b5a08570e63fb
Pandey, Anany <anany.pandey@dell.com>
2023-12-03 21:49:47 +0530

    Container with most water

---
+++ LeetCode/src/main/java/LeetCode_11/Container_With_Most_Water.java
@@ -0,0 +1,29 @@
+package LeetCode_11;
+
+public class Container_With_Most_Water
+{
+    public static int maxArea(int[] height)
+    {
+        int mxArea=Integer.MIN_VALUE;
+        int i=0;
+        int j= height.length-1;
+        while (i<j)
+        {
+            mxArea=Integer.max(mxArea,Integer.min(height[i],height[j])*(j-i));
+            if(height[i]>height[j])
+            {
+                j--;
+            }
+            else if(height[j]>=height[i])
+            {
+                i++;
+            }
+        }
+        return mxArea;
+    }
+    public static void main(String[] args) {
+        System.out.println("Inside Main");
+        int[] arr = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(arr));
+    }
+}
